7d93a780931ced740652a42a354f4d444c8ead3a
Lucas de Sousa Rosa <roses.lucas404@gmail.com>
2022-02-25 16:49:03 -0300

    Update test_all file

--- sched-performance-tester/test_all.py
+++ sched-performance-tester/test_all.py
@@ -2,52 +2,62 @@
 # -*- coding: utf-8 -*-
 import subprocess
 
-subprocess.call(['python test_sched_perfomrance_model256_runtime.py'], shell=True)
+tests = [
+    'test_sched_perfomrance_model256_runtime.py',
+    'test_sched_perfomrance_model1024_runtime.py',
+    'test_sched_perfomrance_model256_estimate.py',
+    'test_sched_perfomrance_model1024_estimate.py'
+    'test_sched_perfomrance_model256_backfilling.py',
+    'test_sched_perfomrance_model1024_backfilling.py',
+    'test_sched_perfomrance_curie_runtime.py',
+    'test_sched_perfomrance_anl_runtime.py',
+    'test_sched_perfomrance_sdscblue_runtime.py',
+    'test_sched_perfomrance_ctcsp2_runtime.py',
+    'test_sched_perfomrance_hpc2n_runtime.py',
+    'test_sched_perfomrance_sdscsp2_runtime.py',
+    'test_sched_perfomrance_curie_estimate.py',
+    'test_sched_perfomrance_anl_estimate.py',
+    'test_sched_perfomrance_sdscblue_estimate.py',
+    'test_sched_perfomrance_ctcsp2_estimate.py',
+    'test_sched_perfomrance_hpc2n_estimate.py',
+    'test_sched_perfomrance_sdscsp2_estimate.py',
+    'test_sched_perfomrance_curie_backfilling.py',
+    'test_sched_perfomrance_anl_backfilling.py',
+    'test_sched_perfomrance_sdscblue_backfilling.py',
+    'test_sched_perfomrance_ctcsp2_backfilling.py',
+    'test_sched_perfomrance_hpc2n_backfilling.py',
+    'test_sched_perfomrance_sdscsp2_backfilling.py'
+]
 
-subprocess.call(['python test_sched_perfomrance_model1024_runtime.py'], shell=True)
+logs = [
+    'model256_runtime',
+    'model1024_runtime',
+    'model256_estimate',
+    'model1024_estimate'
+    'model256_backfilling',
+    'model1024_backfilling',
+    'curie_runtime',
+    'anl_runtime',
+    'sdscblue_runtime',
+    'ctcsp2_runtime',
+    'hpc2n_runtime',
+    'sdscsp2_runtime',
+    'curie_estimate',
+    'anl_estimate',
+    'sdscblue_estimate',
+    'ctcsp2_estimate',
+    'hpc2n_estimate',
+    'sdscsp2_estimate',
+    'curie_backfilling',
+    'anl_backfilling',
+    'sdscblue_backfilling',
+    'ctcsp2_backfilling',
+    'hpc2n_backfilling',
+    'sdscsp2_backfilling'
+]
 
-subprocess.call(['python test_sched_perfomrance_model256_estimate.py'], shell=True)
-
-subprocess.call(['python test_sched_perfomrance_model1024_estimate.py'], shell=True)
-
-subprocess.call(['python test_sched_perfomrance_model256_backfilling.py'], shell=True)
-
-subprocess.call(['python test_sched_perfomrance_model1024_backfilling.py'], shell=True)
-
-subprocess.call(['python test_sched_perfomrance_curie_runtime.py'], shell=True)
-
-subprocess.call(['python test_sched_perfomrance_anl_runtime.py'], shell=True)
-
-subprocess.call(['python test_sched_perfomrance_sdscblue_runtime.py'], shell=True)
-
-subprocess.call(['python test_sched_perfomrance_ctcsp2_runtime.py'], shell=True)
-
-subprocess.call(['python test_sched_perfomrance_hpc2n_runtime.py'], shell=True)
-
-subprocess.call(['python test_sched_perfomrance_sdscsp2_runtime.py'], shell=True)
-
-subprocess.call(['python test_sched_perfomrance_curie_estimate.py'], shell=True)
-
-subprocess.call(['python test_sched_perfomrance_anl_estimate.py'], shell=True)
-
-subprocess.call(['python test_sched_perfomrance_sdscblue_estimate.py'], shell=True)
-
-subprocess.call(['python test_sched_perfomrance_ctcsp2_estimate.py'], shell=True)
-
-subprocess.call(['python test_sched_perfomrance_hpc2n_estimate.py'], shell=True)
-
-subprocess.call(['python test_sched_perfomrance_sdscsp2_estimate.py'], shell=True)
-
-#subprocess.call(['python test_sched_perfomrance_curie_backfilling.py'], shell=True)
-
-subprocess.call(['python test_sched_perfomrance_anl_backfilling.py'], shell=True)
-
-subprocess.call(['python test_sched_perfomrance_sdscblue_backfilling.py'], shell=True)
-
-subprocess.call(['python test_sched_perfomrance_ctcsp2_backfilling.py'], shell=True)
-
-subprocess.call(['python test_sched_perfomrance_hpc2n_backfilling.py'], shell=True)
-
-subprocess.call(['python test_sched_perfomrance_sdscsp2_backfilling.py'], shell=True)
-
+for index, test in enumerate(tests):
+    log_file = logs[index]
+    with open('logs' + log_file + '.log', 'w+') as log:
+        subprocess.call(['python ' + test], shell=True, stdout=log)
 
